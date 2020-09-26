d635b8a4af5022c7f1b3d05c19c464d475592b68
Alvaro Rausell <alvaro.rausellg@gmail.com>
2020-09-26 19:36:41 +0100

    Updates

---
+++ precache-manifest.419db21984ac172dd39e78e9d31e181d.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54dab1f995f085d8c3da6f9bdb832a91",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a58849f516f62ff4e074",
+    "url": "/static/css/2.4f4e799b.chunk.css"
+  },
+  {
+    "revision": "ec383d5609f50bf683b2",
+    "url": "/static/css/main.26d20e0a.chunk.css"
+  },
+  {
+    "revision": "a58849f516f62ff4e074",
+    "url": "/static/js/2.8080d475.chunk.js"
+  },
+  {
+    "revision": "9465d3142f82e5e314c89042673c0333",
+    "url": "/static/js/2.8080d475.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec383d5609f50bf683b2",
+    "url": "/static/js/main.ae40e2ae.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "df48559c8f0a0f368ec361fcddd51a30",
+    "url": "/static/media/Alpaca.df48559c.png"
+  },
+  {
+    "revision": "3e887ff2f936a023178c55866c6488c3",
+    "url": "/static/media/aboutme.3e887ff2.jpg"
+  },
+  {
+    "revision": "d3ae7ba77743154618b55c85af2188ab",
+    "url": "/static/media/btd.d3ae7ba7.jpeg"
+  },
+  {
+    "revision": "f0f8a0e2aa5c906274335a1c5a3d402f",
+    "url": "/static/media/devpost.f0f8a0e2.png"
+  },
+  {
+    "revision": "8a1ad1b3bae4a235a519a7afa63ddd3a",
+    "url": "/static/media/easyNakup.8a1ad1b3.png"
+  },
+  {
+    "revision": "a07c1041087ca75dda52ba454ec534fb",
+    "url": "/static/media/git.a07c1041.png"
+  },
+  {
+    "revision": "5b8f25e2dd3879878392308bd5531a09",
+    "url": "/static/media/hacktrain.5b8f25e2.png"
+  },
+  {
+    "revision": "ef44b758155c73fd134fa09eb10e6b27",
+    "url": "/static/media/iotlab.ef44b758.jpg"
+  },
+  {
+    "revision": "6bb36b30f4fbe3c7b3ee4668cb6de975",
+    "url": "/static/media/linkedin.6bb36b30.png"
+  },
+  {
+    "revision": "bca229635ea4c3c45c221bf3b1f24c75",
+    "url": "/static/media/profile.bca22963.jpg"
+  }
+]);
